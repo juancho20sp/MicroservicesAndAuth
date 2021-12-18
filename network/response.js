19a58c3e9b0526c22b05a27c35c01fd48d5ebd75
Juan David Murillo <juancho20sp@gmail.com>
2021-12-18 16:43:43 -0500

    Initial commit

---
+++ network/response.js
@@ -0,0 +1,24 @@
+// Esto es una muy buena práctica
+// Siempre es bueno dejar las respuestas en un único archivo para tener todo más uniforme
+
+exports.success = function(req, res, message, status) {
+    let statusCode = status || 200;
+    let statusMessage = message || '';
+    
+    res.status(statusCode).send({
+        error: false,
+        status: status,
+        body: statusMessage
+    });
+}
+
+exports.error = function(req, res, message, status) {
+    let statusCode = status || 500;
+    let statusMessage = message || 'Internal server error';
+
+    res.status(statusCode).send({
+        error: true,
+        status: statusCode,
+        body: statusMessage
+    });
+}
